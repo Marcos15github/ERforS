4dfd4bfa03a6239649d680782481ac3180764e2c
Marcos Atencia <marcosatemcia78@gmail.com>
2022-09-07 02:25:27 -0500

    actualizando controlador

--- bin/Controller.js
+++ bin/Controller.js
@@ -55,7 +55,7 @@ class Controller{
             if(err) {
                 res.status(200).send("Error al autenticar Usuario")
             }
-           else if (people == "e"){
+           else if (nickname == "e"){
                 res.status(200).send("No ingreso Usuario")
             }
               else if(!people){
