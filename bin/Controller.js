714d2b7334b362cb4f1b8b06e9fc56e93668a4b4
Marcos Atencia <marcosatemcia78@gmail.com>
2022-09-07 01:08:58 -0500

    routes y controller

--- bin/Controller.js
+++ bin/Controller.js
@@ -54,7 +54,7 @@ class Controller{
             if(err) {
                 res.status(200).send("Error al autenticar Usuario")
             }
-            else if (people.nickname == ""){
+            else if (people == ""){
                 res.status(200).send("No ingreso Usuario")
             }
                else if(!people){
