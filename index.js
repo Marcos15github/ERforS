99f0516050b7518771a18e04e393ff83c8703836
Marcos Atencia <marcosatemcia78@gmail.com>
2022-09-19 16:19:44 -0500

    igual que antes con el login bien

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const {app} = require("./bin/routes");
 const express = require("express");
-
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.listen(PORT, () =>{
     console.log('server on', PORT);
